274b89a0a6f477307889aa75823bd7ea6fe14958
oliviaolip <oliviaoliv2006@gmail.com>
2025-09-27 17:23:03 +0700

    Add files via upload

---
+++ kalkulator.dart
@@ -0,0 +1,23 @@
+import 'dart:core';
+
+class kalkulator {
+  //penjumlahan
+  double tambah(double bil1, double bil2) {
+    return bil1 + bil2;
+  }
+
+  //pengurangan
+  double kurang(double bil1, double bil2) {
+    return bil1 - bil2;
+  }
+
+  //perkalian
+  double kali(double bil1, double bil2) {
+    return bil1 * bil2;
+  }
+
+  //pembagian
+  double bagi(double bil1, double bil2) {
+    return bil1 / bil2;
+  }
+}
